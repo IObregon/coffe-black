3729fe9e0a144f6c8296cdee7531dc653b75008e
epelde <aepelde@gmail.com>
2017-11-07 22:07:31 +0100

    TvShowDetail component

---
+++ src/components/TvShowDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <v-card>
+      <v-card-media
+        v-if="show.backdrop_path"
+        :src="`https://image.tmdb.org/t/p/w500${show.backdrop_path}`"
+        height="250px" />
+      <v-card-media
+        v-else
+        src="http://via.placeholder.com/400x250"
+        height="250px" />
+    </v-card>
+    <v-container fluid>
+      <h5>{{ show.first_air_date }}</h5>
+      <p>{{ show.overview }}</p>
+      <div class="text-xs-left">
+        <v-chip outline color="green accent-2" v-for="genre in show.genres" :key="genre.id">
+          {{ genre.name }}
+        </v-chip>
+      </div>
+    </v-container>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TvShowDetail',
+  props: ['show']
+}
+</script>

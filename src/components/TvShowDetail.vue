0ed4c9918c633e0ad5c0e95070d7e6c4421810e1
epelde <aepelde@gmail.com>
2017-11-08 08:38:49 +0100

    Styling tv show detail

--- src/components/TvShowDetail.vue
+++ src/components/TvShowDetail.vue
@@ -11,15 +11,23 @@
         height="250px" />
     </v-card>
     <v-container fluid>
-      <h5>{{ show.first_air_date }}</h5>
+      <h4>{{ show.first_air_date }}</h4>
       <p>{{ show.overview }}</p>
-      <div class="text-xs-left">
-        <v-chip outline color="green accent-2" v-for="genre in show.genres" :key="genre.id">
-          {{ genre.name }}
-        </v-chip>
-      </div>
-      <p>Seasons: {{ show.number_of_seasons }}</p>
-      <p>Episode: {{ show.number_of_episodes }}</p>
+      <v-layout row>
+        <v-flex xs12 class="text-xs-left">
+          <v-chip outline color="green accent-2" v-for="genre in show.genres" :key="genre.id">
+            {{ genre.name }}
+          </v-chip>
+        </v-flex>
+      </v-layout row>
+      <v-layout row>
+        <v-flex xs6>
+          <h4>Seasons {{ show.number_of_seasons }}</h4>
+        </v-flex>
+        <v-flex xs6>
+          <h4>Episodes {{ show.number_of_episodes }}</h4>
+        </v-flex>
+    </v-layout>
     </v-container>
   </section>
 </template>

84333549b2dc4589c042942e62887ff572c0a5c7
epelde <aepelde@gmail.com>
2017-11-10 12:02:56 +0100

    Implementing first action

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,52 +1,46 @@
 <template>
-<v-container fluid :grid-list-lg="true" :fill-height="loading">
-  <v-layout v-if="loading" justify-center align-center>
-    <v-progress-circular indeterminate color="primary"></v-progress-circular>
-  </v-layout>
-  <v-layout v-else row wrap>
-    <v-flex v-for="it in items" :key="it.id" xs6 sm4 md2 xl1>
-      <v-card :to="{ name: 'detail', params: { id: it.id } }">
-        <v-card-media
-          v-if="it.poster_path"
-          :src="`https://image.tmdb.org/t/p/w500${it.poster_path}`"
-          height="250px"/>
-        <v-card-media
-          v-else
-          src="http://via.placeholder.com/200x250"
-          height="250px"/>
-      </v-card>
-    </v-flex>
-  </v-layout>
-</v-container>
+  <v-container fluid :grid-list-lg="true" :fill-height="loading">
+    <v-layout v-if="loading" justify-center align-center>
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </v-layout>
+    <v-layout v-else row wrap>
+      <v-flex v-for="it in items" :key="it.id" xs6 sm4 md2 xl1>
+        <v-card :to="{ name: 'detail', params: { id: it.id } }">
+          <v-card-media
+            v-if="it.poster_path"
+            :src="`https://image.tmdb.org/t/p/w500${it.poster_path}`"
+            height="250px"/>
+          <v-card-media
+            v-else
+            src="http://via.placeholder.com/200x250"
+            height="250px"/>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-const API_URL = 'https://api.themoviedb.org/3/tv/popular?api_key=f7698770439320b65427198b343fad6f&language=e'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'HomePage',
-  data () {
-    return {
-      loading: true,
-      items: []
+  created () {
+    if (this.items.length === 0) {
+      this.init()
     }
   },
-  created () {
-    this.fetchData()
-  },
   methods: {
-    fetchData () {
-      const that = this
-      fetch(`${API_URL}`)
-        .then(res => {
-          return res.json()
-        })
-        .then(function (json) {
-          if (json.results.length > 0) {
-            that.loading = false
-            that.items = json.results
-          }
-        })
+    ...mapActions({
+      init: 'loadPopularItems'
+    })
+  },
+  computed: {
+    loading () {
+      return this.items.length === 0
+    },
+    items () {
+      return this.$store.state.popularItems
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const initialState = {
+  popularItems: []
+}
+
 export default new Vuex.Store({
-  state: {
-
-  },
+  state: initialState,
   mutations: {
-
+    loadPopularItems (state, items) {
+      state.popularItems = [
+        ...state.popularItems,
+        ...items
+      ]
+    }
   },
   actions: {
-
+    loadPopularItems ({ commit }) {
+      fetch('https://api.themoviedb.org/3/tv/popular?api_key=f7698770439320b65427198b343fad6f')
+        .then(res => {
+          return res.json()
+        })
+        .then(function (json) {
+          if (json.results.length > 0) {
+            commit('loadPopularItems', json.results)
+          }
+        })
+    }
   }
 })

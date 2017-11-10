c9d5f58f53a586dcfed399d649bfbfe63099ccec
epelde <aepelde@gmail.com>
2017-11-10 12:19:14 +0100

    Making grid list smaller

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-container fluid :grid-list-lg="true" :fill-height="loading">
+  <v-container fluid :grid-list-md="true" :fill-height="loading">
     <v-layout v-if="loading" justify-center align-center>
       <v-progress-circular indeterminate color="primary"></v-progress-circular>
     </v-layout>
     <v-layout v-else row wrap>
-      <v-flex v-for="it in items" :key="it.id" xs6 sm4 md2 xl1>
+      <v-flex v-for="it in items" :key="it.id" xs4 sm4 md2 xl1>
         <v-card :to="{ name: 'detail', params: { id: it.id } }">
           <v-card-media
             v-if="it.poster_path"
             :src="`https://image.tmdb.org/t/p/w500${it.poster_path}`"
-            height="250px"/>
+            height="200"/>
           <v-card-media
             v-else
             src="http://via.placeholder.com/200x250"
-            height="250px"/>
+            height="200"/>
         </v-card>
       </v-flex>
     </v-layout>

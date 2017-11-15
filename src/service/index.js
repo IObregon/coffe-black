bd50ce69cea9f3a4318667e74695fc684009a896
epelde <aepelde@gmail.com>
2017-11-15 22:12:09 +0100

    Including page parameter with default value

--- src/service/index.js
+++ src/service/index.js
@@ -3,8 +3,8 @@ import API_KEY from './API_KEY'
 const API_URL = 'https://api.themoviedb.org/3/tv'
 
 export default {
-  getPopularItems () {
-    return fetch(`${API_URL}/popular?api_key=${API_KEY}`)
+  getPopularItems (page = 1) {
+    return fetch(`${API_URL}/popular?api_key=${API_KEY}&page=${page}`)
   },
   getItem (id) {
     return fetch(`${API_URL}/${id}?api_key=${API_KEY}`)

11855565001b794efb3264b33f62060c1813ed2a
epelde <aepelde@gmail.com>
2017-11-15 14:06:51 +0100

    Shared component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app dark>
-    <v-container v-if="showProgress" fill-height align-center>
-       <v-layout justify-center align-center>
-         <v-progress-circular indeterminate color="red darken-2"></v-progress-circular>
-       </v-layout>
-    </v-container>
+    <progress-indicator :display="showProgress"></progress-indicator>
     <router-view></router-view>
   </v-app>
 </template>
@@ -12,8 +8,13 @@
 <script>
 import { mapState } from 'vuex'
 
+import ProgressIndicator from './shared/components/progress_indicator'
+
 export default {
   name: 'App',
+  components: {
+    ProgressIndicator
+  },
   computed: {
     ...mapState(['showProgress'])
   }
---
+++ src/shared/components/progress_indicator.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container v-if="display" fill-height align-center>
+     <v-layout justify-center align-center>
+       <v-progress-circular indeterminate color="red darken-2">
+       </v-progress-circular>
+     </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ProgressIndicator',
+  props: ['display']
+}
+</script>
